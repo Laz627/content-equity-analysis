e32a484cbb4a1df110d975ef14a726b3f82d818c
Laz627 <169095963+Laz627@users.noreply.github.com>
2024-06-17 15:39:36 -0400

    Update code.py

--- code.py
+++ code.py
@@ -64,9 +64,10 @@ def main():
     st.header("How to Use It")
     st.write("""
     1. **Download the Template**: Click the 'Download Equity Analysis Template XLSX' button to download a template file with the required columns.
-    2. **Upload Your Data**: Use the 'Upload your XLSX file' button to upload your equity analysis data and keywords in separate tabs.
-    3. **Select Columns**: Choose which columns to include in the analysis from the multiselect dropdown.
-    4. **View Results**: The app will display the analyzed results and provide a download link for the final output file.
+    2. **Fill Out the Template**: Enter your values into the template XLSX file. If values aren't applicable, either enter 0 for all empty fields, or delete the header columns.
+    3. **Select Columns**: Choose which columns to include in the analysis from the multiselect dropdown. Remove any columns that are empty / unused.
+    4. **Upload Your Data**: Use the 'Upload your XLSX file' button to upload your equity analysis data and keywords in separate tabs.
+    5. **View Results**: The app will display the analyzed results and provide a download link for the final output file.
     """)
 
     if st.button("Download Equity Analysis Template XLSX"):
